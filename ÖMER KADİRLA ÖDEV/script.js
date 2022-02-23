824a5a1d5add15604cc42845eff27de233f7beb2
omerkadirla <97515987+omerkadirla@users.noreply.github.com>
2022-02-23 15:55:58 +0300

    Add files via upload

---
+++ ÖMER KADİRLA ÖDEV/script.js
@@ -0,0 +1,39 @@
+document.getElementById("Ulke").addEventListener("change",yer)
+let sonuc = document.getElementById("sonucBaslık")
+
+function yer(){
+      sonuc.classList.remove("ABD","TR","UK","RUS","normal")
+      let Ulke=document.getElementById(Ulke).selectedIndex
+
+      if(ulke==1){
+      sonuc.innerHTML="Nüfus:350 milyon"
+      sonuc.classList.add("ABD")
+      document.getElementById("logo").setAttribute("src","amerika.png")
+
+      }
+      if(ulke==2){
+        sonuc.innerHTML="Nüfus:81 milyon"
+        sonuc.classList.add("TR")
+        document.getElementById("logo").setAttribute("src","amerika.png")
+  
+        }
+        if(ulke==3){
+            sonuc.innerHTML="Nüfus:44 milyon"
+            sonuc.classList.add("UK")
+            document.getElementById("logo").setAttribute("src","amerika.png")
+      
+            }
+      
+            if(ulke==4){
+                sonuc.innerHTML="Nüfus:144 milyon"
+                sonuc.classList.add("RUS")
+                document.getElementById("logo").setAttribute("src","amerika.png")
+          
+                }
+               console.log(ulke);
+            }
+            function merhaba(){
+                  let ad
+
+                ad=document.getElementById("txtad").value
+            }
